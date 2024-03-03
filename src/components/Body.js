e6a862874fae12d719c2f3454b92b7098285de99
Ranjodh Singh <ranjodhsing24@gmail.com>
2024-03-03 00:53:36 -0500

    Remove mock data and use swiggy live data with fetch and useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,26 @@
-import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Body = () => {
 
     // Local State variable - using Hooks
-    // const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-    // above one line of code is same as below three lines of code
-    const arr = useState(resList);
-    const listOfRestaurants = arr[0];
-    const setListOfRestaurants = arr[1];
+    // this hook takes two arguments
+    // 1. callback function
+    // 2. list of dependencies
+    // this hook is called after the body component is rendered
+    
+    useEffect(() => {
+        fetchData();
+    },[]);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5/?lat=12.9351929&lng=77.6244809999&page_type=DESKTOP_WEB_LISTING");
+
+        const json = await data.json();
+        console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    }
 
     return(
         <div className="body">
@@ -19,7 +30,7 @@ const Body = () => {
                     className="filter-btn"
                     onClick={()=>{
                         //Filter logic for top rated restaurant
-                        const filterListOfRestaurants = listOfRestaurants.filter((res) => res.rating.text > 4);
+                        const filterListOfRestaurants = listOfRestaurants.filter((res) => res.info.avgRating > 4.2);
                         setListOfRestaurants(filterListOfRestaurants);
                         console.log(filterListOfRestaurants);
                     }}
@@ -30,7 +41,7 @@ const Body = () => {
                     className="clear-btn"
                     onClick={()=>{
                         //Filter logic for top rated restaurant
-                        setListOfRestaurants(resList);
+                        fetchData();
                     }}
                 >
                     Clear Filter
@@ -39,7 +50,7 @@ const Body = () => {
             <div className="res-container">
                 {
                     listOfRestaurants.map((restaurant => 
-                    <RestaurantCard key={restaurant.storeUuid} resData={restaurant} />))
+                    <RestaurantCard key={restaurant.info.id} resData={restaurant} />))
                 }
             </div>
     </div>)
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 const HEADER_LOGO = 
     "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
 
+const IMAGE_BASE_URL = "https://media-assets.swiggy.com/swiggy/image/upload/"
 // Named export
-export { HEADER_LOGO };
+export { HEADER_LOGO, IMAGE_BASE_URL};
