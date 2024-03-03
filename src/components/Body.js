252929e32edf0ca41d0aadfa94f6c6f4510fe874
Ranjodh Singh <ranjodhsing24@gmail.com>
2024-03-03 02:43:49 -0500

    Add search functionality and how to keep the actual list intact

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,14 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { SWIGGY_API } from "../utils/constants";
+
 const Body = () => {
 
     // Local State variable - using Hooks
     const [listOfRestaurants, setListOfRestaurants] = useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+    const [searchText, setSearchText] = useState("");
 
     // this hook takes two arguments
     // 1. callback function
@@ -16,23 +20,41 @@ const Body = () => {
     },[]);
 
     const fetchData = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5/?lat=12.9351929&lng=77.6244809999&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch(SWIGGY_API);
 
         const json = await data.json();
         console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
         setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
 
     return(
         <div className="body">
-            <div className="search">Search</div>
             <div className="filter">
+                <div className="search">
+                    <input 
+                        type="text"
+                        value={searchText}
+                        onChange = { (e) => {
+                            setSearchText(e.target.value);
+                        }}
+                        className="search-box"
+                        placeholder="Search Restaurant"  
+                    />
+                    <button onClick={()=>{
+                        // Filter the search data
+                        let filteredList = listOfRestaurants.filter((res)=> 
+                            res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                        );
+                        setFilteredRestaurants(filteredList);
+                    }}>Search</button>
+                </div>
                 <button
                     className="filter-btn"
                     onClick={()=>{
                         //Filter logic for top rated restaurant
                         const filterListOfRestaurants = listOfRestaurants.filter((res) => res.info.avgRating > 4.2);
-                        setListOfRestaurants(filterListOfRestaurants);
+                        setFilteredRestaurants(filterListOfRestaurants);
                         console.log(filterListOfRestaurants);
                     }}
                 >
@@ -42,7 +64,7 @@ const Body = () => {
                     className="clear-btn"
                     onClick={()=>{
                         //Filter logic for top rated restaurant
-                        fetchData();
+                        setFilteredRestaurants(listOfRestaurants);
                     }}
                 >
                     Clear Filter
@@ -52,7 +74,7 @@ const Body = () => {
                 {
                     listOfRestaurants.length === 0 
                     ? <Shimmer />
-                    : listOfRestaurants.map((restaurant => 
+                    : filteredRestaurants.map((restaurant => 
                     <RestaurantCard key={restaurant.info.id} resData={restaurant} />))
                 }
             </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,17 @@
 const HEADER_LOGO = 
     "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
 
-const IMAGE_BASE_URL = "https://media-assets.swiggy.com/swiggy/image/upload/"
+const IMAGE_BASE_URL = 
+    "https://media-assets.swiggy.com/swiggy/image/upload/"
+
+// swiggy API to get restruants from Bangalore (based on lat long)
+// const SWIGGY_API = 
+    // "https://www.swiggy.com/dapi/restaurants/list/v5/?lat=12.9351929&lng=77.6244809999&page_type=DESKTOP_WEB_LISTING";
+
+// swiggy API to get restruants from Ludhiana (based on lat long)
+const SWIGGY_API = 
+    "https://www.swiggy.com/dapi/restaurants/list/v5/?lat=30.870860&lng=75.832464&page_type=DESKTOP_WEB_LISTING";
+
+
 // Named export
-export { HEADER_LOGO, IMAGE_BASE_URL};
+export { HEADER_LOGO, IMAGE_BASE_URL, SWIGGY_API};
