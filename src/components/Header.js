079bb14448541ca6ee30a33b3954a1b6a936a919
Ranjodh Singh <ranjodhsing24@gmail.com>
2024-03-03 01:54:29 -0500

    Using useState to render the particular component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
 import { HEADER_LOGO } from '../utils/constants';
+import { useState } from "react";
 const Header = () => {
+    const [btnName, setBtnName] = useState("Login");
+    // NOTE: everytime setBtnName is called whole component is re-rendered and const value of
+    // btnName is reset to whatever the new value is passed in setBtnName
+    // this is the reason why even const variable is updated on state method call in React
+    // But based on the Reconcilliation algorithm only the button is refreshed but everything stays
+    // the same on this component
+    console.log("logged in");
     return (
         <div className="header">
             <div className="logo-container">
@@ -11,6 +19,13 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="login-btn" onClick={()=>{ 
+                        btnName==="Login" 
+                        ? setBtnName("Logout")
+                        : setBtnName("Login");
+                        }}>
+                            {btnName}
+                    </button>
                 </ul>
             </div>
         </div>
